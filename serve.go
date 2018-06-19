bf4e5534b1b131556bc0f4a616240177badf4840
JK Gunnink <jgunnink@gmail.com>
2018-06-19 16:01:20 +0800

    Refactor serve.go

--- serve.go
+++ serve.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
 	log.Printf("Serving %s on HTTP port: %s\n", ".", "8080")
-	log.Fatal(http.ListenAndServe(":"+"8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
